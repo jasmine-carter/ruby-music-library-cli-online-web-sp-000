e58f2efb545f86a497676c8a62c66ad781d33938
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-14 23:25:22 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -43,7 +43,7 @@ class MusicLibraryController
       puts "Please enter the name of an artist:"
       input = gets.chomp
       Artist.all.select do |artist| artist.name == input
-        puts artist.songs.to_a
+        puts artist.songs
       end
     end
 
