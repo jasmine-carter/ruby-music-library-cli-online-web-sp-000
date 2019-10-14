7130d537553ff7eed6ba5713a84d79acce708b5a
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-14 23:44:00 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
         input = gets
       Artist.all.each do |artist| artist.name == input
         artist.songs.sort {|a, b| a.name <=>b.name}.each.with_index(1) do |song, index|
-          puts "#{index}. #{song.name} - #{song.genre.name}"
+          #puts "#{index}. #{song.name} - #{song.genre.name}"
         end
       end
     end
