67f8f24d4b16a24b818a6845661c24dd334432ca
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-14 23:27:20 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -40,7 +40,7 @@ class MusicLibraryController
     end
 
     def list_songs_by_artist
-      puts "Please enter the name of an artist:"
+    puts "Please enter the name of an artist:"
       input = gets
       Artist.all.each do |artist| artist.name == input
         puts artist.songs
