53a8ea3c92a1a2c79a95e63fdb4eec27d2ea1b7f
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-14 23:20:04 +0000

    Automatically backed up by Learn

---
+++ lib/musiclibrarycontroller.rb
@@ -0,0 +1,46 @@
+class MusicLibraryController
+
+  def initialize(path="./db/mp3s")
+    new_import = MusicImporter.new(path)
+    new_import.import
+  end
+
+  def call
+    input = ' '
+    while input != "exit"
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    input = gets.chomp
+    end
+  end
+
+    def list_songs
+      Song.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+
+    def list_artists
+      Artist.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |artist, index|
+        puts "#{index}. #{artist.name}"
+      end
+    end
+
+    def list_genres
+      Genre.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |genre, index|
+        puts "#{index}. #{genre.name}"
+      end
+    end
+
+    def list_songs_by_artist
+      Artist.all
+    end
+
+end
