20eb0d8685accff5eabfc768d6c65933c023500d
Jasmine Carter <jasmine.carter87@gmail.com>
2019-10-15 00:52:29 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -27,6 +27,10 @@ class MusicLibraryController
         list_genres
       when 'list artist'
         list_songs_by_artist
+      when 'list genre'
+        list_songs_by_genre
+      when 'play song'
+        play_song
       end
     end
   end
